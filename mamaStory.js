267b31c12507b2467d68f037a9c9bbdcc091bb83
thelittlecitizen <thelittlecitizen7@gmail.com>
2020-09-15 11:58:34 -0700

    finish

--- mamaStory.js
+++ mamaStory.js
@@ -1,16 +1,3 @@
-class BaseWeapon{
-    demage;
-    type;
-    constructor(demage , type){
-        this.demage = demage;
-        this.type = type
-    }
-
-    Attack(){
-        return "Hit";
-    }
-}
-
 
 // weapons
 
@@ -96,7 +83,7 @@ class Player{
     }
 
     Attack(){
-        return "Name" + name + " Attacked with " + this.baseWeapon.demage + " Demage! " + this.baseWeapon.Attack();
+        return "Name " + this.name + " Attacked with " + this.baseWeapon.demage + " Demage! " + this.baseWeapon.Attack();
     }
 }
 
@@ -145,8 +132,9 @@ function createPlayer(){
         var playerType = playerTypeSelect.options[playerTypeSelect.selectedIndex].value;
 
 
-        var player = playerFactory(playerType,weaponFactory(weaponType),name,genderType);
+        var player = playerFactory(playerType,weaponFactory(weaponType),name.value,genderType);
         addPlayer(player);
+        renderPlayers();
         if (player == null){
             throw "player type is not found : " + playerType
         }
@@ -157,10 +145,13 @@ function createPlayer(){
 function playerFactory(playerType ,weapon, name ,gender){
     switch(playerType){
         case "Fighter":
+            console.log("1")
             return new Fighter(weapon,gender,name);
         case "Magican":
+            console.log("2")
             return new Magican(weapon,gender,name);
         case "Rainbow":
+            console.log("3")
             return new Rainbow(weapon,gender,name);
         default:
             return null;
@@ -186,5 +177,33 @@ function weaponFactory(weapnType){
 
 
 function renderPlayers(){
+    var htmlDiv = "<div class='row justify-content-center'>";
+    var playersContainers = document.getElementById("allPlayers");
+    playerList.forEach((player) => {
+        htmlDiv += getPlayerHtml(player)
+        
+    });
+    htmlDiv += "</div>"
+    playersContainers.innerHTML = htmlDiv;
 
+}
+
+function getPlayerHtml(player){
+
+   var htmlDiv = ""
+    htmlDiv += "<div class='col-md-4  text-center w-200'>"
+    htmlDiv += "<div class='card h-100'>"
+    htmlDiv += "<div class='card-body'>"
+    htmlDiv += "<h1> name : "+player.name+"</h1>"
+    htmlDiv += "<h1> gender : "+player.gender+"</h1>"
+    htmlDiv +=`<input type="button" value="create player" onClick="playerAttack('${player.Attack()}')" />`
+    htmlDiv += "</div>"
+    htmlDiv += "</div>"
+    htmlDiv += "</div>"
+    return htmlDiv;
+}
+
+
+function playerAttack(str){
+    alert(str)
 }
